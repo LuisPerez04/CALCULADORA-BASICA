044055aa2171bffc3f41bede4ae3fe8a308f3464
Atomi <Atomi@LAPTOP-2PMC52BP>
2021-05-16 17:15:23 -0500

    Calculadora2

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,6 +1,19 @@
 package calculadora;
+/**
+ * 
+ * @author Luis Roman Perez Martinez
+ * @since 16 de Mayo 2021
+ * @version 1.0.2
+ */
 public class Calculadora {
+    /**
+     * @param args es mi metodo main
+     */
     public static void main(String[] args) {
+        /**
+     * 
+     * Mostrar resultado
+     */
        calculator micalculadora = new calculator (21,76);
        int suma = micalculadora.sumar();
        System.out.println("La suma es: " + suma);
--- src/calculadora/calculator.java
+++ src/calculadora/calculator.java
@@ -1,36 +1,80 @@
 package calculadora;
+/**
+ * 
+ * @author Luis Roman Perez Martinez
+ * @since 16 de Mayo 2021
+ * @version 1.0.2
+ */
 public class calculator {
-    //Atributos
+    /**
+     * 
+     * Declaracion de mis varibalbes y atributos
+     */
     private int operando1;
     private int operando2;
     private String signo;
     public double resultado;
     private int resultadoC;
     private int numero;
-    //Constructores
+    
+    /**
+     * 
+     * Mi metodo constructor
+     */
     public calculator(){
 }
+    /**
+     * Metodo inicial de la calculadora
+     * @param operando1
+     * @param operando2 
+     * Guarda un valor inicial si el usuario necesita hacer alguna operacion aritmetica
+     * 
+     */
     public calculator (int operando1, int operando2){
         this.operando1 = operando1;
         this.operando2 = operando2;
     }
-    //Metodos
+    /**
+     * Apartado Suma
+     * Recorre el metodo para sumar los numeros introducidos en los parametros designados
+     * @return 
+     */
     public int sumar(){
         int suma = this.operando1 + this.operando2;
         return suma;
     }
+    /**
+     * Apartado Resta 
+     * Recorre el metodo para restar los numeros que se introduzcan en el parametro
+     * @return 
+     */
     public int restar(){
         int resta = this.operando1 - this.operando2;
         return resta;
     }
+    /**
+     * Apartado Multiplicacion
+     * Recorre el metodo para multiplicar los numeros que se introduzcan en el parametro
+     * @return 
+     */
     public int multiplicar (){
         int multiplicacion = this.operando1 * this.operando2;
         return multiplicacion;
     }
+    /**
+     * Apartado Division
+     * Recorre el metodo para multiplicar los numeros que se introduzcan en el parametro y si
+     * se divide en 0 te menciona numero infinito
+     * @return 
+     */
     public double dividir (){
         double division = (double) this.operando1 / (double)this.operando2;
         return division;
     }
+    /**
+     * Apartado de get y set de operandos
+     * @return 
+     */
     public int getOperando1() {
         return operando1;
     }
@@ -46,7 +90,10 @@ public class calculator {
     public void setOperando2(int operando2) {
         this.operando2 = operando2;
     }
-
+     /**
+     * 
+     * Elimina la ultima operacion realizada
+     */
     public void eliminar(){
         if(this.signo.equalsIgnoreCase("+")){
             this.resultado -= this.numero;
@@ -58,7 +105,10 @@ public class calculator {
             this.resultado *= this.numero;
         }
     }
-    
+    /**
+     * 
+     * Mostrar resultado
+     */
     public void resultado(){
         resultadoC = (int)resultado;
         if(resultado % resultado == 0){
